2978a004fe9acb146b3026e4d052e9c3c5ee9466
shivamdesai1208 <shivamdesai128@gmail.com>
2026-01-11 22:40:53 +0530

    Encapsulation code

--- Encapsulation/Demo3.java
+++ Encapsulation/Demo3.java
@@ -1,8 +1,35 @@
+class Student
+{
+    private int rollNumber;
+    private String name;
+
+
+    public int getRollNumber() 
+    {
+        return rollNumber;
+    }
+    public void setRollNumber(int rollNumber) 
+    {
+        this.rollNumber = rollNumber;
+    }
+    public String getName() 
+    {
+        return name;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    
+}
+
+
 public class Demo3 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello ");
+        System.out.println("Encapsulation example");
         
     }
     
