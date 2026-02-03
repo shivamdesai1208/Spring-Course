eb1d0f26dd25b500916c53d0846a8d72d0bfb05d
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-03 13:37:54 +0530

    Whole code added

--- Encapsulation/Account.java
+++ Encapsulation/Account.java
@@ -3,7 +3,7 @@ class Details
     private long accountNumber;
     private long accountBalance;
     private String accountHolderName;
-    
+
     public long getAccountNumber() {
         return accountNumber;
     }
@@ -28,6 +28,18 @@ public class Account
 {
     public static void main(String a[])
     {
+        Details D1 = new Details();
+
+        D1.setAccountBalance(10000);
+        D1.setAccountNumber(1001);
+        D1.setAccountHolderName("Shivam");
+
+        System.out.println("Account Details are as follows - ");
+        System.out.println("Ac Number : " + D1.getAccountNumber());
+        System.out.println("Ac Holder : " + D1.getAccountHolderName());
+        System.out.println("Balance   : " + D1.getAccountBalance());
+
+
         System.out.println("Account project");
     }
     
