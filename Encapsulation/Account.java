1e67401bbf67b98c36cb53f1838e8aa7f9c680a2
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-03 13:32:06 +0530

    Getter Setter added

--- Encapsulation/Account.java
+++ Encapsulation/Account.java
@@ -1,8 +1,34 @@
+class Details
+{
+    private long accountNumber;
+    private long accountBalance;
+    private String accountHolderName;
+    
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public long getAccountBalance() {
+        return accountBalance;
+    }
+    public void setAccountBalance(long accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+}
+
 public class Account 
 {
     public static void main(String a[])
     {
-        System.out.println("Account priject");
+        System.out.println("Account project");
     }
     
 }
