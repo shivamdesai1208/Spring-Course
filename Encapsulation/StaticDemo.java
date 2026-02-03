43d334898cc7861a3f0a65290d0ea828285d1934
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-03 17:01:57 +0530

    Use of static keyword with class name

--- Encapsulation/StaticDemo.java
+++ Encapsulation/StaticDemo.java
@@ -19,12 +19,12 @@ public class StaticDemo
         Phones P1 = new Phones();
         P1.brand = "Apple";
         P1.price = 1500;
-        P1.name = "Phone";
+        Phones.name = "Phone";
 
         Phones P2 = new Phones();
         P2.brand = "Samsung";
         P2.price = 1700;
-        P2.name = "Smart Phone";
+        Phones.name = "Smart Phone";
 
         P1.show();
         P2.show();
