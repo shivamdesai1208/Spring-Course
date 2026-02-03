f992e3f2337229fe7aca958040c71cdf5c551c53
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-03 16:50:10 +0530

    Using default values

--- Encapsulation/Account.java
+++ Encapsulation/Account.java
@@ -4,6 +4,20 @@ class Details
     private long accountBalance;
     private String accountHolderName;
 
+    public Details()
+    {
+        accountBalance = 1111;
+        accountNumber = 1111;
+        accountHolderName = "AAAA";
+    }
+
+    public Details(long aN, long aB, String hN)
+    {
+        accountBalance = aB;
+        accountNumber = aN;
+        accountHolderName = hN;
+    }
+
     public long getAccountNumber() {
         return accountNumber;
     }
@@ -29,18 +43,23 @@ public class Account
     public static void main(String a[])
     {
         Details D1 = new Details();
+        Details D2 = new Details(1001, 10000, "Shivam");
 
-        D1.setAccountBalance(10000);
-        D1.setAccountNumber(1001);
-        D1.setAccountHolderName("Shivam");
+        // D1.setAccountBalance(10000);
+        // D1.setAccountNumber(1001);
+        // D1.setAccountHolderName("Shivam");
 
         System.out.println("Account Details are as follows - ");
         System.out.println("Ac Number : " + D1.getAccountNumber());
         System.out.println("Ac Holder : " + D1.getAccountHolderName());
         System.out.println("Balance   : " + D1.getAccountBalance());
 
+        System.out.println("------------------------------------------------");
 
-        System.out.println("Account project");
+        System.out.println("Ac Number : " + D2.getAccountNumber());
+        System.out.println("Ac Holder : " + D2.getAccountHolderName());
+        System.out.println("Balance   : " + D2.getAccountBalance());
+
     }
     
 }
