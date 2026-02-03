b6c84bcfc5519e4d47dddc9e7f116a1099bf4a6f
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-03 13:24:13 +0530

    Student getter-setter

--- Encapsulation/Demo4.java
+++ Encapsulation/Demo4.java
@@ -1,7 +1,34 @@
+class Student
+{
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
+
 public class Demo4 
 {
     public static void main (String a[])
 {
+    Student S1 = new Student();
+
+    S1.setId(1001);
+    S1.setName("Shivam");
+
+    System.out.println(S1.getId());
+    System.out.println(S1.getName());
 
     System.out.println("Hello");
 
