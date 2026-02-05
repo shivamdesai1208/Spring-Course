2d569603543647fab89506aa344da8c28cb07c29
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-05 18:22:59 +0530

    very advance calculator

--- Inheritance/DemoCalc.java
+++ Inheritance/DemoCalc.java
@@ -4,14 +4,15 @@ public class DemoCalc
     public static void main(String[] args) 
     {
 
-        AdvCalc C1 = new AdvCalc();
+        VeAdvCalc C1 = new VeAdvCalc();
 
         int r1 = C1.add(4,5);
         int r2 = C1.sub(5,4);
         int r3 = C1.div(5,4);
         int r4 = C1.mul(5,4);
+        double r5 = C1.power(4,2);
 
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
     
 }
