808c4d8effcd6ee192510977a2a58236fbcd7a74
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-05 21:06:39 +0530

    New line

--- Inheritance/DemoABC.java
+++ Inheritance/DemoABC.java
@@ -2,7 +2,14 @@ class A
 {
     public A()
     {
+        super();
         System.out.println("in A");
+    }
+
+    public A(int a)
+    {
+        super();
+        System.out.println("in A int");
     }
 
 
@@ -12,10 +19,26 @@ class B extends A
 {
     public B()
     {
+        super();
         System.out.println("in B");
     }
 
+    public B(int a)
+    {
+        this();
+        System.out.println("in B int");
+    }
+
 }
-public class DemoABC {
+public class DemoABC 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+
+        B obj = new B(12);
+
+        
+    }
     
 }
