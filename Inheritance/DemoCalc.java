5c730e753228dff1f16d0007b630545b4c14b3ec
shivamdesai1208 <shivamdesai128@gmail.com>
2026-02-05 18:17:32 +0530

    Inheritance

--- Inheritance/DemoCalc.java
+++ Inheritance/DemoCalc.java
@@ -1,22 +1,17 @@
-class Calc
-{
-
-    public void show()
-    {
-        System.out.println("Inside show method");
-    }
-
-}
 
 public class DemoCalc 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Inside main method");
-        Calc C1 = new Calc();
-        C1.show();
 
-        
+        AdvCalc C1 = new AdvCalc();
+
+        int r1 = C1.add(4,5);
+        int r2 = C1.sub(5,4);
+        int r3 = C1.div(5,4);
+        int r4 = C1.mul(5,4);
+
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
     }
     
 }
